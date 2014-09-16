112707d0733ab15a2b2f95bf106c5c147990bac4
Sandeep Tamhankar <sandman@electric-cloud.com>
2014-09-15 17:04:50 -0700

    Added tests

---
+++ app/src/androidTest/java/com/example/sandeep/test2/ItemDetailActivityTest.java
@@ -0,0 +1,34 @@
+package com.example.sandeep.test2;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class ItemDetailActivityTest extends ActivityInstrumentationTestCase2<ItemDetailActivity> {
+
+    private ItemDetailActivity mActivity;
+
+    public ItemDetailActivityTest() {
+        super(ItemDetailActivity.class);
+    }
+
+    public void testOnCreate() throws Exception {
+        assertEquals("fake test failure", "expected", "actual");
+    }
+
+    public void testUI() throws Exception {
+        mActivity.runOnUiThread( new Runnable() {
+            @Override
+            public void run() {
+                mActivity.findViewById(R.id.item_detail).requestFocus();
+            }
+        });
+
+        Thread.sleep(5000);
+    }
+
+    @Override protected void setUp() throws Exception {
+        super.setUp();
+
+        mActivity = getActivity();
+
+    }
+}
